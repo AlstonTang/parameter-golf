e62da0d4ff7fb10e9200424af270c14a7c7a3a7f
Alston Tang <145715945+AlstonTang@users.noreply.github.com>
2026-04-21 15:43:10 -0700

    warmup plus slight increase of qk_gain and mlp_mult

--- train_gpt.py
+++ train_gpt.py
@@ -54,11 +54,11 @@ class Hyperparameters:
     # Training length.
     iterations = int(os.environ.get("ITERATIONS", 20000))
     warmdown_iters = int(os.environ.get("WARMDOWN_ITERS", 512))
-    warmup_steps = int(os.environ.get("WARMUP_STEPS", 128))
+    warmup_steps = int(os.environ.get("WARMUP_STEPS", 192))
     train_batch_tokens = int(os.environ.get("TRAIN_BATCH_TOKENS", 524_288))
     train_seq_len = int(os.environ.get("TRAIN_SEQ_LEN", 1024))
     max_wallclock_seconds = float(os.environ.get("MAX_WALLCLOCK_SECONDS", 600.0))
-    qk_gain_init = float(os.environ.get("QK_GAIN_INIT", 1.25))
+    qk_gain_init = float(os.environ.get("QK_GAIN_INIT", 2))
 
     # Model shape.
     vocab_size = int(os.environ.get("VOCAB_SIZE", 1024))
@@ -66,7 +66,7 @@ class Hyperparameters:
     num_kv_heads = int(os.environ.get("NUM_KV_HEADS", 8))
     model_dim = int(os.environ.get("MODEL_DIM", 512))
     num_heads = int(os.environ.get("NUM_HEADS", 8))
-    mlp_mult = float(os.environ.get("MLP_MULT", 1.4))
+    mlp_mult = float(os.environ.get("MLP_MULT", 1.5))
     tie_embeddings = bool(int(os.environ.get("TIE_EMBEDDINGS", "1")))
     rope_base = float(os.environ.get("ROPE_BASE", 4096.0))
 
